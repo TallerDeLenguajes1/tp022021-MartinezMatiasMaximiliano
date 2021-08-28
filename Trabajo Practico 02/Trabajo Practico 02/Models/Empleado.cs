2ae9888a438e52a7d43992be9616870ce9c6f02f
matias maximiliano martinez <matumartinez9@hotmail.com>
2021-08-27 21:07:00 -0300

    Iniciado, Creado clase Empleado, Creado formulario en Index

---
+++ Trabajo Practico 02/Trabajo Practico 02/Models/Empleado.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trabajo_Practico_02.Models
+{
+    public class Empleado
+    {
+        private static float salarioBasico = 5000;
+        private static float descuento = 0.15f;
+
+        private string nombre;
+        private string apellido;
+        private string direccion;
+        private DateTime fechaNacimiento;
+        private int edad;
+        private DateTime fechaIngreso;
+        private int antiguedad;
+        private float salario;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public DateTime FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
+        public int Antiguedad { get => antiguedad; set => antiguedad = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public float Salario { get => salario; set => salario = value; }
+
+        public Empleado(string _Nombre, string _Apellido, string _Direccion, DateTime _FechaNacimiento, DateTime _FechaIngreso)
+        {
+            nombre = _Nombre;
+            apellido = _Apellido;
+            direccion = _Direccion;
+            fechaNacimiento = _FechaNacimiento;
+            fechaIngreso = _FechaIngreso;
+            antiguedad = CalcularAntiguedad();
+            edad = CalcularEdad();
+            salario = CalcularSalario();
+        }
+
+        private int CalcularAntiguedad() { return (DateTime.Now - this.fechaIngreso).Days / 365; }
+
+        private int CalcularEdad() { return (DateTime.Now - this.fechaNacimiento).Days / 365; }
+
+        private float CalcularSalario()
+        {
+            float adicional = 0;
+            if (antiguedad < 20)
+            {
+                adicional = salarioBasico * (antiguedad / 100f);
+            }
+            else
+            {
+                adicional = salarioBasico * 0.25f;
+            }
+            float salario = (salarioBasico + adicional) - (salarioBasico * -descuento);
+            return salario;
+        }
+    }
+}
+
---
+++ Trabajo Practico 02/Trabajo Practico 02/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Trabajo_Practico_02.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
